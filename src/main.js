e8b029990910936242da774234f90c4a3cbdfba3
Yaroslav <example@gmail.com>
2020-03-31 20:56:35 +0300

    Вставил пропущенные точку с запятой

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ const createTabsTemplate = () => {
       <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
       <a class="trip-tabs__btn" href="#">Stats</a>
     </nav>`
-  )
+  );
 };
 
 const createFiltersTemplate = () => {
