11df5cbdba2b6f5ff6bdf6bf0b2cedd3fcd91c6c
Yaroslav <example@gmail.com>
2020-04-28 13:42:23 +0300

    Создал модель для эвентов

--- src/mock/events.js
+++ src/mock/events.js
@@ -86,7 +86,8 @@ const generateEvent = () => {
     photos: generatePhotos(),
     startDate: Math.min(startDate, endDate),
     endDate: Math.max(startDate, endDate),
-    isFavorite: Math.random() > 0.5
+    isFavorite: Math.random() > 0.5,
+    id: String(new Date() + Math.random())
   };
 };
 
